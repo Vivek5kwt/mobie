09e0d05cf4969330c70de01d38ff18af053fba32
Vivek5kwt <vivek5kwt@gmail.com>
2026-01-10 18:35:51 +0530

    Update layouts query usage

--- src/graphql/queries/layoutVersionQuery.js
+++ src/graphql/queries/layoutVersionQuery.js
@@ -1,32 +1,32 @@
 import { gql } from "@apollo/client";
 
 const LAYOUT_VERSION_QUERY = gql`
-  query Layout($appId: Int) {
-    layout(app_id: $appId) {
+  query Layouts($appId: Int) {
+    layouts(app_id: $appId) {
       app_id
+      layout_versions {
+        dsl
+        app_id
+        layout_id
+        created_at
+        id
+        is_published
+        metadata
+        page_name
+        published_by
+        store_id
+        updated_at
+        version_number
+      }
       created_at
       current_version_id
       handle
       id
-      updated_at
-      template_id
-      store_id
-      page_name
       is_published
-      layout_versions {
-        version_number
-        updated_at
-        store_id
-        published_by
-        page_name
-        metadata
-        layout_id
-        is_published
-        id
-        dsl
-        created_at
-        app_id
-      }
+      page_name
+      store_id
+      template_id
+      updated_at
     }
   }
 `;
