3eca2d449686d60cdf790576332b465e1cd760d8
Vivek5kwt <vivek5kwt@gmail.com>
2026-01-12 14:35:56 +0530

    Fix redux provider setup

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import SplashScreen from "./src/screens/saplash";
 import LayoutScreen from './src/screens/LayoutScreen';
@@ -16,6 +18,7 @@ import CheckoutWebViewScreen from "./src/screens/CheckoutWebViewScreen";
 import CollectionProductsScreen from "./src/screens/CollectionProductsScreen";
 import AllProductsScreen from "./src/screens/AllProductsScreen";
 import { AuthProvider } from "./src/services/AuthContext";
+import { persistor, store } from "./src/store";
 
 import tokenLogger from './src/utils/tokenLogger';
 import messaging from '@react-native-firebase/messaging';
@@ -149,14 +152,16 @@ export default function App() {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <AuthProvider>
-          <ApolloProvider client={client}>
-            <NavigationContainer>
-              <Stack.Navigator
-                screenOptions={{ headerShown: false }}
-                initialRouteName="Splash"
-              >
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaProvider>
+            <AuthProvider>
+              <ApolloProvider client={client}>
+                <NavigationContainer>
+                  <Stack.Navigator
+                    screenOptions={{ headerShown: false }}
+                    initialRouteName="Splash"
+                  >
 
               {/* 🔥 First Screen: Splash */}
               <Stack.Screen name="Splash" component={SplashScreen} />
@@ -206,11 +211,13 @@ export default function App() {
                 options={{ animation: "slide_from_right" }}
               />
 
-              </Stack.Navigator>
-            </NavigationContainer>
-          </ApolloProvider>
-        </AuthProvider>
-      </SafeAreaProvider>
+                  </Stack.Navigator>
+                </NavigationContainer>
+              </ApolloProvider>
+            </AuthProvider>
+          </SafeAreaProvider>
+        </PersistGate>
+      </Provider>
     </GestureHandlerRootView>
   );
 }
