2cbbd2cc66f7f96ba33f76d173b1532165d31cc4
vivek sharma <vivek5kwt@gmail.com>
2026-02-04 11:29:49 +0530

    commite till 04-02-26

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: "https://mobidrag.ampleteck.com/graphql",
+    uri: "https://app.mobidrag.com/graphql",
   }),
   cache: new InMemoryCache(),
 });
